77c74e834d49cdfc96c7b41cafea1c0f86929de4
Bacuuu <823727097@qq.com>
2020-11-21 15:03:13 +0800

    store:待处理sass样式隔离

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import React, { useEffect } from 'react';
 import { throttle } from 'throttle-debounce'
 import './App.css';
 import PhotoView from './components/PhotoView'
+import SimplePhoto from './components/simplePhoto'
 import photo from './color.png'
 function App() {
   const [photoVisible, setVisible] = React.useState(false);
-
+  const [howmanyPerrow, setPerrow] = React.useState(4)
   
   useEffect(() => {
     window.addEventListener('resize', handleResize) //监听滚动
   }, [])
 
-  const handleResize = throttle(100, false, (e: any) => {
-    console.log(e.currentTarget.innerHeight, e.currentTarget.innerWidth)
+  const handleResize = throttle(500, false, (e: any) => {
+    const width = e.currentTarget.innerWidth;
+    if ( width > 1500) {
+      setPerrow(4)
+    } else if (width > 1000) {
+      setPerrow(3)
+    } else {
+      setPerrow(2)
+    }
   })
 
   function toggleImg () {
@@ -25,10 +33,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={toggleImg}></button>
+        <SimplePhoto
+        photo={photo}
+        howManyPerRow={howmanyPerrow}
+        onClick={toggleImg}/>
         <PhotoView
           photoImages={[photo, photo, photo]}
-          photoNow={6}
+          photoNow={0}
           photoVisible={photoVisible}
           onClose={photoClose}
           ></PhotoView>
--- src/components/simplePhoto/index.tsx
+++ src/components/simplePhoto/index.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import './photo.scss'
 interface typeProps {
   photo: string,
-  howManyPerRow: number
+  howManyPerRow: number,
+  onClick():any
 }
 
 function SimplePhoto(props: typeProps) {
-  return {
-    // type
-  }
+  return (
+    <div onClick={props.onClick} className={`simple-photo width-${props.howManyPerRow}`} >
+      <img className="photo" src={props.photo} alt=""/>
+    </div>
+  )
 }
 
 export default SimplePhoto;
